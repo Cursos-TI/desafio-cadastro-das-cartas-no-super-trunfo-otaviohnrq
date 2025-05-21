713f967bb6a1e4b03a8cf774c96dd10851383719
Otávio Henrique <otaviohenriqdev@gmail.com>
2025-05-21 00:13:35 +0000

    Criei printf com nome do desafio.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,7 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    printf("Desafio Cartas Super Trunfo!");
+
     return 0;
 }
